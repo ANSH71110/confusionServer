88cd8ad4fbde5af88f24d06bb40a40cfbb4b5057
ANSH71110 <anshbale11@gmail.com>
2022-03-09 17:55:37 +0530

    REST API with Express, MongoDB and Mongoose Part 1

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,11 +1,83 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+
+const Dishes=require('../models/dishes');
 
 var dishRouter=express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
+.get((req,res,next)=>{
+    Dishes.find({})
+    .then((dishes)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dishes);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.post((req,res,next)=>{
+    Dishes.create(req.body)
+    .then((dish)=>{
+        console.log('Dish created:',dish);
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.put((req,res,next)=>{
+    res.statusCode=403;
+    res.end('PUT operation not supported for /dishes/');
+})
+.delete((req,res,next)=>{
+    Dishes.remove({})
+    .then((resp)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+});
+dishRouter.route('/:dishId/')
+.get((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.post((req,res,next)=>{
+    res.statusCode=403;
+    res.end('POST operation not supported for /dishes/'
+    +req.params.dishId);   
+})
+.put((req,res,next)=>{
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set:req.body
+    },{new:true})
+    .then((dish)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));    
+})
+.delete((req,res,next)=>{
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+});
+module.exports=dishRouter;
+/*dishRouter.route('/')
 .all( (req,res,next)=>{
     res.statusCode=200;
     res.setHeader('context-type','text/plain');
@@ -47,4 +119,4 @@ dishRouter.route('/:dishId/')
 .delete((req,res,next)=>{
     res.end('Deleting the dish:'+req.params.dishId);
 });
-module.exports=dishRouter;
+module.exports=dishRouter;*/
